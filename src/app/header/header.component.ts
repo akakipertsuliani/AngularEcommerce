203aabea8810766b6bdadb84b4ffb6292dc3be86
akakipertsuliani <akaki@pertsuliani>
2024-07-05 01:15:21 +0400

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
+// header.component.ts
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { MatMenuModule, MatMenuTrigger, MatMenu } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../servise/auth.service';
-import { Observable, map } from 'rxjs';
 
 @Component({
     selector: 'app-header',
@@ -26,18 +26,13 @@ export class HeaderComponent {
     public isUserDropDown: boolean = false;
     isUserHere: boolean = false;
 
-    @ViewChild(MatMenuTrigger) trigger?: MatMenuTrigger;
+    @ViewChild("menu") trigger?: MatMenuTrigger;
+    @ViewChild("categories") categories?: MatMenuTrigger;
 
     constructor(private auth: AuthService, private route: Router) {
         this.auth.isUserAuth().subscribe(data => {
             this.isUserHere = data;
         })
-    }
-
-    someMethod() {
-        if (this.trigger) {
-            this.trigger.openMenu();
-        }
     }
 
     userLogout() {
